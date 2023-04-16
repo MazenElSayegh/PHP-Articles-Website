24c659c3311a91f8174cbbd2f7979a847e0593f8
Ahmed Tahoun <ahmed.tahoun1997@gmail.com>
2023-04-16 03:20:15 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tahoun/arricles',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9ef2b22b6943fa02e359d05b93598ebc39641edf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tahoun/arricles' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9ef2b22b6943fa02e359d05b93598ebc39641edf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
