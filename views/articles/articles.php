dd932f422a31b8f9279c9686992329a6e9578285
Ahmed Fahmi <amfahmi.96@gmail.com>
2023-04-16 23:02:14 +0200

    editing articles

--- views/articles/articles.php
+++ views/articles/articles.php
@@ -1,6 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
 <?php
+    require_once("controllers/articles.php");
     echo "<div id=container class='d-inline-flex flex-column justify-content-center align-items-center m-5'><div id=formCont><form action=".$_SERVER['PHP_SELF']." method=GET>";
     echo "<input type=search name=article_search class='border border-1 border-primary rounded pl-1' placeholder=Product Name>";
     echo "<button type=submit class='bg-primary border border-1 border-primary rounded text-light mx-4'>Search</button></form></div>";
