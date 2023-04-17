b01a10ca84043eb2f27a964103489b46e9b686c9
Ahmed Tahoun <ahmed.tahoun1997@gmail.com>
2023-04-17 23:33:44 +0200

    updating dev

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 define("__HOST__","localhost");
 define("__USER__","root");
 define("__PASS__","");
-define("__DB__","adminpanel");
+define("__DB__","Articles");
+define("SITE_FOLDER","PHP-Articles-Website");
 define("__RECORDS_PER_PAGE__",4);
 define("__Debug__Mode__",0);
 ?>
